31be64b94f7f19ea7dc2ff108fe4574725a6729d
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-06-26 14:25:10 +0530

    added the images + the inference code for the embeding model

--- PRODUCTION/Task_B/inference/verify_face.py
+++ PRODUCTION/Task_B/inference/verify_face.py
@@ -3,6 +3,11 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model  # type: ignore
 from tensorflow.keras.preprocessing.image import load_img, img_to_array  # type: ignore
 
+'''
+Please add the embedding_sequel.h5 model in the PRODUCTION/models/ directory
+to get the embedding model followup to this URL in the github repo release
+https://github.com/Circuit-Overtime/CNN_vedic/releases/tag/publish101
+'''
 # ======= Configuration =======
 EMBEDDING_MODEL_PATH = "PRODUCTION/models/embedding_sequel.h5"
 THRESHOLD = 0.945  
@@ -22,7 +27,7 @@ def preprocess_image(img_path, target_size=IMG_SIZE):
 def get_embedding(image_path):
     image = preprocess_image(image_path)
     embedding = embedding_model.predict(image)[0]
-    return embedding / np.linalg.norm(embedding)  # L2-normalization (optional if not already normalized)
+    return embedding / np.linalg.norm(embedding)  
 
 # ======= Match Function =======
 def is_match(reference_path, test_path, threshold=THRESHOLD):
@@ -34,8 +39,15 @@ def is_match(reference_path, test_path, threshold=THRESHOLD):
 
 # ======= Example Usage =======
 if __name__ == "__main__":
-    reference_img = r"E:\CNN_vedic\Data\Task_A\TESTING\unseen\trey1.png"
-    test_img = r"E:\CNN_vedic\Data\Task_A\TESTING\unseen\trey2.png"
+
+    # For match found between the two images
+    reference_img = r"PRODUCTION/Task_B/test/0/0.jpg"
+    test_img = r"PRODUCTION/Task_B/test/0/distortion/0 (1).jpg"
+
+    # For no match found between the two images
+    # reference_img = r"PRODUCTION/Task_B/test/2/2.jpg"
+    # test_img = r"PRODUCTION/Task_B/test/3/distortion/3 (1).jpg"
+
 
     if is_match(reference_img, test_img):
         print("✅ MATCH: Same identity")
